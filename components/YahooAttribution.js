d310bad686993faf57e7ff7fb3f1b727bd2561c5
Jared <bennattj@gmail.com>
2026-08-31 12:47:00 -0500

    Added yahoo branding where applicable.

--- components/YahooAttribution.js
+++ components/YahooAttribution.js
@@ -11,7 +11,7 @@ export default function YahooAttribution() {
             target="_blank"
             rel="noopener noreferrer"
             className={styles.attributionLink}
-            aria-label="Fantasy data provided by Yahoo Fantasy"
+            aria-label="Real time draft data provided by Yahoo Fantasy"
         >
             <span className={styles.logoCrop}>
                 <img
@@ -20,7 +20,7 @@ export default function YahooAttribution() {
                     className={styles.logo}
                 />
             </span>
-            <span className={styles.text}>Fantasy data provided by Yahoo Fantasy</span>
+            <span className={styles.text}>Real time draft data provided by Yahoo Fantasy</span>
         </a>
     )
 }
